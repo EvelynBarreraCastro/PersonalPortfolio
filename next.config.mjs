294f15ff4bf573e7e324acfb99430aae2a037075
Evelyn Barrera Castro <165227051+EvelynBarreraCastro@users.noreply.github.com>
2024-07-11 01:34:14 -0700

    Attempting to add the "/out" to path

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', 
-    images: { 
-        unoptimized: true, 
-    }, 
-    assetPrefix: process.env.ASSET_PREFIX,
-    basePath: process.env.BASE_PATH,
-};
+//const nextConfig = {
+  //  output: 'export', 
+   // images: { 
+   //     unoptimized: true, 
+    //}, 
+   // assetPrefix: process.env.ASSET_PREFIX,
+   // basePath: process.env.BASE_PATH,
+//};
+//export default nextConfig;
 
-export default nextConfig;
+const config = {
+    basePath: '/PersonalPortfolio/out',
+    assetPrefix: '/PersonalPortfolio/out/',
+  };
+  
+  export default config;
+  
