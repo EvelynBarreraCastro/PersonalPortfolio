0f255b2ab88770b480ded91a97cdb8f0ae80ec7c
Evelyn Barrera Castro <165227051+EvelynBarreraCastro@users.noreply.github.com>
2024-07-12 09:30:31 -0700

    Update next.config.js
    reverting back to original code

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ const nextConfig = {
    images: { 
        unoptimized: true, 
     }, 
-   //assetPrefix: process.env.ASSET_PREFIX,
-    //basePath: process.env.BASE_PATH,
-    basePath: '/PersonalPortfolio/out',
-    assetPrefix: '/PersonalPortfolio/out/',
+   assetPrefix: process.env.ASSET_PREFIX,
+   basePath: process.env.BASE_PATH,
+    //basePath: '/PersonalPortfolio/out',
+    //assetPrefix: '/PersonalPortfolio/out/',
 };
 export default nextConfig;
